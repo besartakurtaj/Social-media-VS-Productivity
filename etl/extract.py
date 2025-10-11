dd674df86bfd1c1f2a6bf80d41aee6f976f9c5e2
Besarta Kurtaj <besarta.kurtaj@student-uni-pr.edu>
2025-10-11 21:14:48 +0200

    Check details about our dataset

--- etl/extract.py
+++ etl/extract.py
@@ -2,3 +2,22 @@ import pandas as pd
 
 def extract_data(file_path: str) -> pd.DataFrame:
     df = pd.read_csv(file_path)
+
+    print("Data extracted")
+    print(f"Shape: {df.shape}")
+    print("\nFirst 5 rows:")
+    print(df.head())
+
+    print("\n DataFrame Info:")
+    print(df.info())
+
+    print("\n Missing Values per Column:")
+    print(df.isnull().sum())
+
+    duplicates = df.duplicated().sum()
+    print(f"\n Duplicate Rows: {duplicates}")
+
+    print("\n Descriptive Statistics:")
+    print(df.describe())
+
+    return df
